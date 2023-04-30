b0c634faa42b8f27064059aa92c7651d1c0da434
Ayush Agrawal <87698717+Pegasus321@users.noreply.github.com>
2023-04-30 23:50:59 +0530

    functions

---
+++ App.js
@@ -0,0 +1,8 @@
+const const hey = name =>`hey man ${name}`;
+const lol = hey('sujay');
+console.log(lol);
+
+const const hey = name =>{
+return `hey man ${name}`};
+const lol = hey('sujay');
+console.log(lol);
